730d975c4a856ca934449382b2e973721c5aa089
Jack Beeken <beekenj@gmail.com>
2018-11-27 17:32:38 -0700

    Implemented basic functions from book

--- mm.c
+++ mm.c
@@ -36,9 +36,9 @@
  ********************************************************/
 team_t team = {
   /* Team name */
-  "",
+  "Jack Beeken",
   /* First member's full name */
-  "",
+  "beekenj@gmail.com",
   /* First member's email address */
   "",
   /* Second member's full name (leave blank if none) */
@@ -59,6 +59,9 @@ team_t team = {
 #define DSIZE       8       /* doubleword size (bytes) */
 #define CHUNKSIZE  (1<<12)  /* initial heap size (bytes) */
 #define OVERHEAD    8       /* overhead of header and footer (bytes) */
+// Start my macros
+#define MAX_HEAP    16
+// End
 
 static inline int MAX(int x, int y) {
   return x > y ? x : y;
@@ -121,6 +124,9 @@ static inline void* PREV_BLKP(void *bp){
 //
 
 static char *heap_listp;  /* pointer to first block */  
+// Start my vars
+
+// End
 
 //
 // function prototypes for internal helper routines
@@ -140,6 +146,17 @@ int mm_init(void)
   //
   // You need to provide this
   //
+  // Create the initial empty heap
+  if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) return 1;
+  PUT(heap_listp, 0);                                           // Alignment Padding
+  PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));                  // Prologue header
+  PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));                  // Prologue footer
+  PUT(heap_listp + (3*WSIZE), PACK(0, 1));                  // Epilogue header
+  heap_listp += (2*WSIZE);
+
+  // Extend the empty heap with a free block of CHUNKSIZE bytes
+  if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
+
   return 0;
 }
 
@@ -149,10 +166,18 @@ int mm_init(void)
 //
 static void *extend_heap(uint32_t words) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+  char *bp; 
+  size_t size;
+  /* Allocate an even number of words to maintain alignment */
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  if ((long)(bp = mem_sbrk(size)) == -1)
+      return NULL;
+  /* Initialize free block header/footer and the epilouge header*/
+  PUT(HDRP(bp), PACK(size,0)); /* Free block header */
+  PUT(FTRP(bp), PACK(size,0)); /* Free block footer */
+  PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); /* New Epilouge header */
+  /* Coalesce if the previous block was free */
+  return coalesce(bp);
 }
 
 
@@ -171,9 +196,12 @@ static void *find_fit(uint32_t asize)
 //
 void mm_free(void *bp)
 {
-  //
-  // You need to provide this
-  //
+  size_t size = GET_SIZE(HDRP(bp));
+
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+  coalesce(bp);
+
 }
 
 //
@@ -181,6 +209,33 @@ void mm_free(void *bp)
 //
 static void *coalesce(void *bp) 
 {
+  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+  if (prev_alloc && next_alloc) {         // Case 1
+      return bp;
+  }
+
+  else if (prev_alloc && !next_alloc) {   // Case 2
+      size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+      PUT(HDRP(bp), PACK(size, 0));
+      PUT(FTRP(bp), PACK(size, 0));
+  }
+
+  else if (!prev_alloc && next_alloc) {   // Case 3
+      size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+      PUT(FTRP(bp), PACK(size, 0));
+      PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+      bp = PREV_BLKP(bp);
+  }
+
+  else {                                  // Case 4
+      size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+      PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+      PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+      bp = PREV_BLKP(bp);
+  }
   return bp;
 }
 
@@ -192,7 +247,32 @@ void *mm_malloc(uint32_t size)
   //
   // You need to provide this
   //
-  return NULL;
+  size_t asize;         // Adjusted block size
+  size_t extendsize;    // Amount to extend the heap if no fit
+  char *bp;
+
+  // Ignore squrious requests
+  if (size == 0)
+      return NULL;
+
+  // Adjust block size to include overhead and alignment reqs.
+  if (size <= DSIZE)
+      asize = 2*DSIZE;
+  else
+      asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+  // Search the free list for a fit
+  if ((bp = find_fit(asize)) != NULL) {
+      place(bp, asize);
+      return bp;
+  }
+
+  // No fit found. Get more memory and place the block
+  extendsize = MAX(asize, CHUNKSIZE);
+  if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+      return NULL;
+  place(bp, asize);
+  return bp;
 } 
 
 //
